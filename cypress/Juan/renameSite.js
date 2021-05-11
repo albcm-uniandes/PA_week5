a3f42a8c85bc822daf0600227bf748e76ece6c21
Juan Acevedo <78027763+jacevedob@users.noreply.github.com>
2021-05-10 23:08:27 -0500

    Cypress renameSite.js

---
+++ cypress/Juan/renameSite.js
@@ -0,0 +1,28 @@
+/// <reference types="cypress" />
+
+context('Actions', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:2368/ghost/#/signin')
+  })
+
+  it('Cambier el titulo del sitio', () => {
+    cy.get('[id^=login]').within(() => {
+      cy.get('.email.ember-text-field.gh-input.ember-view').type('acevedobedoya@gmail.com')
+      cy.get('.password.ember-text-field.gh-input.ember-view').type('abcd1234*+')
+      cy.get('.login.gh-btn.gh-btn-blue.gh-btn-block.gh-btn-icon.ember-view').click()
+      cy.log('Login successful')
+    })
+
+    cy.wait(200)
+    cy.get('[href="#/settings/general/"]').click({ force: true })
+    cy.wait(200)
+    cy.get('.gh-setting-first .gh-setting-action .gh-btn').contains('Expand').first().click({ force: true })
+    cy.wait(200)
+    cy.get('.gh-setting-first .gh-setting-content .ember-text-field').first().clear({ force: true })
+    cy.wait(200)
+    cy.get('.gh-setting-first .gh-setting-content .ember-text-field').first().type('Nuevo titulo para el sitio',{ force: true })
+    cy.wait(200)
+    cy.get('.gh-btn.gh-btn-blue').click()
+  })
+
+})

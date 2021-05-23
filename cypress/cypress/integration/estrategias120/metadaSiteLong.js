f1faaeab1e0220371a747affcb55d3ab29b367c0
Juan Acevedo <78027763+jacevedob@users.noreply.github.com>
2021-05-23 17:04:02 -0500

    Pruebas Juan Acevedo

---
+++ cypress/cypress/integration/estrategias120/metadaSiteLong.js
@@ -0,0 +1,38 @@
+const faker = require('faker')
+const data = require('./data')
+const util = require('../login.js')
+
+const cookieSessionName =
+  Cypress.env("cookieSessionName") || "ghost-admin-api-session";
+
+context('Actions', () => {
+
+  before(() => {
+    util.login();
+  })
+
+  beforeEach(() => {
+    Cypress.Cookies.preserveOnce(cookieSessionName);
+  })
+
+  it('Configuraciones generales', () => {
+    cy.get('[href="#/settings/general/"]').click({ force: true })
+    cy.wait(2000)
+  })
+
+  it('Cambiar los metadatos para los buscadores', () => {
+    cy.get('.gh-setting-first .gh-setting-action .gh-btn').contains('Expand').first().click({ force: true })
+    cy.wait(2000)
+
+    cy.get('.gh-setting-first .gh-setting-content .ember-text-field').last().clear({ force: true })
+    cy.wait(2000)
+
+    cy.get('.gh-setting-first .gh-setting-content .ember-text-field').last().type(faker.lorem.sentences(35),{ force: true })
+    cy.wait(2000)
+  })
+
+  it('Guardar los cambios', () => {
+    cy.get('.gh-btn.gh-btn-blue').click()
+    cy.wait(2000)
+  })
+})
---
+++ cypress/cypress/integration/estrategias120/privateSpace.js
@@ -0,0 +1,38 @@
+
+const faker = require('faker')
+const data = require('./data')
+const util = require('../login.js')
+
+const cookieSessionName =
+  Cypress.env("cookieSessionName") || "ghost-admin-api-session";
+
+context('Actions', () => {
+
+  before(() => {
+    util.login();
+  })
+
+  beforeEach(() => {
+    Cypress.Cookies.preserveOnce(cookieSessionName);
+  })
+
+  it('Configuraciones generales', () => {
+    cy.get('[href="#/settings/general/"]').click({ force: true })
+    cy.wait(2000)
+  })
+
+  it('Volver privada la publicación', () => {
+    cy.get('.input-toggle-component').click({ force: true })
+    cy.wait(2000)
+    cy.get('input[name="general[password]"]').clear({ force: true })
+    cy.wait(2000)
+    cy.get('input[name="general[password]"]').type(data.space, { force: true })
+    cy.wait(2000)
+  })
+
+  it('Guardar los cambios', () => {
+    cy.get('.gh-btn.gh-btn-blue').click()
+    cy.wait(2000)
+  })
+})
+  
